39bdc9681d4fd0f543319fa50e3f3756c1f18714
SquidsHeart <54671600+SquidsHeart@users.noreply.github.com>
2022-12-04 16:12:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ import server
 server.main()
 client.init_call(config.BROADCAST_IP_ADDRESS, config.PORT)
 
-print(client.start_convo("10.4.97.8"))
+
+
+
+
+### This is an example of how communication between 2 computers can be started. This line of code creates a connection between 'device_ip' and this device
+### This makes 'device_ip' connect to a TCP server created by the server file. What is done at this point can be chosen by the user
+
+device_ip = '127.0.0.1'
+client.start_convo(device_ip)
